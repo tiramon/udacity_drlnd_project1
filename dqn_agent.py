9dd85c34c09e3c472a5bc4a459e0276921c1159a
tiramon <github@checkspam.de>
2020-05-26 14:29:57 +0200

    remove prints of network in dqn agent

--- dqn_agent.py
+++ dqn_agent.py
@@ -36,8 +36,6 @@ class Agent():
         # Q-Network
         self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
         self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
-        print(self.qnetwork_local)
-        print(self.qnetwork_target)
         self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
 
         # Replay memory
